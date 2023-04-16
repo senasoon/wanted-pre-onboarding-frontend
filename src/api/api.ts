d7174b9074c05ccabc5a36fa2bd5487001e01bdc
senasoon <senasoon0@gmail.com>
2023-04-16 23:36:31 +0900

    feat: 로그인 기능 구현

--- src/api/api.ts
+++ src/api/api.ts
@@ -3,6 +3,9 @@ import { Auth } from '../types/auth';
 
 const api = axios.create({ baseURL: process.env.REACT_APP_BASE_URL });
 
+export const signIn = ({ email, password }: Auth) => {
+  return api.post('/auth/signin', { email, password });
+};
 export const signUp = ({ email, password }: Auth) => {
   api.post('/auth/signup', { email, password });
 };
--- src/pages/SignInPage.tsx
+++ src/pages/SignInPage.tsx
@@ -2,6 +2,8 @@ import { useNavigate } from 'react-router-dom';
 import FormInput from '../components/form/FormInput';
 import useInput from '../hooks/useInput';
 import styles from '../assets/scss/components/Form.module.scss';
+import { useEffect, useState } from 'react';
+import { signIn } from '../api/api';
 
 const SignInPage = () => {
   const navigate = useNavigate();
@@ -9,19 +11,55 @@ const SignInPage = () => {
   const { value: email, onChange: emailChangeHandler } = useInput('');
   const { value: password, onChange: passwordChangeHandler } = useInput('');
 
+  const [isValidEmail, setIsValidEmail] = useState(false);
+  const [isValidPassword, setIsValidPassword] = useState(false);
+
+  useEffect(() => {
+    email.includes('@') && email.includes('.') ? setIsValidEmail(true) : setIsValidEmail(false);
+  }, [email]);
+
+  useEffect(() => {
+    password.length >= 8 ? setIsValidPassword(true) : setIsValidPassword(false);
+  }, [password]);
+
+  const onSubmitHandler = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    try {
+      const { data } = await signIn({ email, password });
+      localStorage.setItem('access_token', data['access_token']);
+      navigate('/todo');
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <>
       <h1 className={styles.heading1}>로그인</h1>
-      <form className={styles.form}>
-        <FormInput labelText="이메일" id="email" testId="email-input" value={email} onChange={emailChangeHandler} />
+      <form className={styles.form} onSubmit={onSubmitHandler}>
+        <FormInput
+          labelText="이메일"
+          id="email"
+          testId="email-input"
+          value={email}
+          onChange={emailChangeHandler}
+          placeholder="example@example.com"
+        />
         <FormInput
           labelText="비밀번호"
           id="password"
           testId="password-input"
           value={password}
           onChange={passwordChangeHandler}
+          placeholder="8자 이상"
+          type="password"
+          autoComplete="off"
         />
-        <button className={styles.disabledButton} data-testid="signin-button" disabled>
+        <button
+          className={isValidEmail && isValidPassword ? styles.activeButton : styles.disabledButton}
+          data-testid="signin-button"
+          disabled={isValidEmail && isValidPassword ? false : true}
+        >
           로그인
         </button>
         <button
